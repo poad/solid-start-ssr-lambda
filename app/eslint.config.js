a8eea345c185aa51ee2b18992474eadde977cd87
Kenji Saito <ken-yo@mbr.nifty.com>
2025-03-05 22:02:35 +0900

    Bumps node modules

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -18,7 +18,7 @@ import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
+const gitignorePath = path.resolve(__dirname, "../.gitignore");
 
 export default tseslint.config(
   includeIgnoreFile(gitignorePath),
